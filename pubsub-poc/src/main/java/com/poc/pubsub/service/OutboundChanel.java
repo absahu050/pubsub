1bee02b9123f2a43fe5e76cfd98a1c9b9d9c9a40
Abhishek Kumar Sahu <72202288+absahu050@users.noreply.github.com>
2025-04-03 11:55:11 +0530

    Delete pubsub-poc directory

--- pubsub-poc/src/main/java/com/poc/pubsub/service/OutboundChanel.java
+++
@@ -1,8 +0,0 @@
-package com.poc.pubsub.service;
-
-import org.springframework.integration.annotation.MessagingGateway;
-
-@MessagingGateway(defaultRequestChannel = "outboundMessageChannel")
-public interface OutboundChanel {
-	void sendMsgToPubSub(String msg);
-}
--- pubsub-poc/src/main/resources/static/js/pubsub.js
+++
@@ -1,5 +0,0 @@
-jQuery(function() {
-	// console.log('inside index.html page load');
-
-	// jQuery('.datatable').DataTable();
-});
